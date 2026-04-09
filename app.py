883755cf595cb6aa2afe158d3c1160a7b0e6a2c7
Surya S <suryarsuresh1258@gmail.com>
2026-04-09 23:28:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def dashboard():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
